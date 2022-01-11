34ffd2168cd4f6d7c62c1bbfbf0cb2c111498059
welbon.wang <welbon.wang@gmail.com>
2022-01-11 10:07:30 +0800

    NEW: add trait example

---
+++ exam-trait/src/main.rs
@@ -0,0 +1,44 @@
+#[macro_use]
+extern crate impls;
+
+trait Actor {
+    fn actor(&self);
+}
+
+trait Health {
+    fn health(&self);
+}
+
+struct Plant;
+
+impl Actor for Plant {
+    fn actor(&self) {
+        println!("Plant Actor");
+    }
+}
+
+struct Monster {
+    health: f32,
+}
+
+impl Actor for Monster {
+    fn actor(&self) {
+        println!("Monster Actor");
+    }
+}
+
+impl Health for Monster {
+    fn health(&self) {
+        println!("Health: {}", self.health);
+    }
+}
+
+fn main() {
+    // let plant = Box::new(Plant);
+    // let monster = Box::new(Monster { health: 100f32 });
+    if impls!(Plant: Actor) {
+        println!("Plant is a type of Actor");
+    } else {
+        println!("not found");
+    }
+}
